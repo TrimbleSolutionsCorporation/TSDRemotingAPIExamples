3a2190f3d2cc3ed450d0ddff1b4949fc6336c22a
Venca-M <64779742+Venca-M@users.noreply.github.com>
2023-08-02 11:38:35 +0200

    Add ExtractingResultsTableItem example

---
+++ ExtractingResultsTableItem/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TSD.API.Remoting;
+using TSD.API.Remoting.Structure;
+
+namespace ExtractingResultsTableItem
+{
+	static class Program
+	{
+		[STAThread]
+		public static async Task Main()
+		{
+			// Get the first running TSD instance found
+			using( var tsdInstance = await ApplicationFactory.GetFirstRunningApplicationAsync() )
+			{
+				if( tsdInstance == null )
+				{
+					Console.WriteLine( "No running instances of TSD found!" );
+
+					return;
+				}
+
+				// Output the version number of TSD
+				Console.WriteLine( await tsdInstance.GetVersionStringAsync() );
+
+				// Get the active document from the running instance of TSD
+				var document = await tsdInstance.GetDocumentAsync();
+
+				if( document == null )
+				{
+					Console.WriteLine( "No document was found in the TSD instance!" );
+
+					return;
+				}
+
+				// Get the model from the document
+				var model = await document.GetModelAsync();
+
+				if( model == null )
+				{
+					Console.WriteLine( "No model was found in the document!" );
+
+					return;
+				}
+
+				var member = await GetMemberForOutput( model );
+
+				if( member == null )
+				{
+					Console.WriteLine( "No member found." );
+
+					return;
+				}
+
+				var lineItemString = GetLineItemStringFromUserInput();
+
+				var resultValuesOutputter = new ResultsValuesOutputter( member, lineItemString, tsdInstance );
+				await resultValuesOutputter.Output();
+
+				Console.ReadLine();
+			}
+		}
+
+		private static async Task<IMember> GetMemberForOutput( IModel model )
+		{
+			var members = await model.GetMembersAsync();
+
+			IMember member = null;
+
+			do
+			{
+				Console.WriteLine( "Enter member reference:" );
+
+				string memberName = Console.ReadLine();
+
+				member = members.FirstOrDefault( m => m.Name == memberName );
+
+				if( member == null )
+				{
+					Console.WriteLine( "Member named " + memberName + " not found" );
+				}
+			} while( member == null );
+
+			return member;
+		}
+
+		private static string GetLineItemStringFromUserInput()
+		{
+			string lineItemString;
+
+			do
+			{
+				Console.WriteLine( "Enter string to use for results table search:" );
+				lineItemString = Console.ReadLine();
+			} while( lineItemString.Length == 0 );
+
+			return lineItemString;
+		}
+	}
+}
---
+++ ExtractingResultsTableItem/ResultsValuesOutputter.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TSD.API.Remoting;
+using TSD.API.Remoting.Common;
+using TSD.API.Remoting.Report;
+using TSD.API.Remoting.Structure;
+using TSD.API.Remoting.Units;
+
+namespace ExtractingResultsTableItem
+{
+	public class ResultsValuesOutputter
+	{
+		#region Constructors
+
+		public ResultsValuesOutputter( IMember member, string lineItemString, IApplication tsdInstance )
+		{
+			LineItemString = lineItemString;
+			Member = member;
+			UnitConverter = tsdInstance.UnitConverter;
+		}
+
+		#endregion
+
+		#region Properties
+
+		private string LineItemString { get; }
+
+		private IMember Member { get; }
+
+		private IUnitConverter UnitConverter { get; }
+
+		#endregion
+
+		#region Methods
+
+		public async Task Output()
+		{
+			var results = await Member.GetCalculationsAsync( CheckResultType.Static );
+
+			await ProcessCalculationsNodeList( results.Nodes, "" );
+			await ProcessItems( results.Items, "" );
+		}
+
+		private async Task ProcessCalculationsNodeList( IReadOnlyList<ICalculationsNode> nodes, string hierarchyString )
+		{
+			if( !nodes.Any() )
+			{
+				return;
+			}
+
+			foreach( var node in nodes )
+			{
+				string hierarchyStringNode = hierarchyString + " > " + node.Name;
+
+				await ProcessCalculationsNodeList( node.Nodes, hierarchyStringNode );
+				await ProcessItems( node.Items, hierarchyStringNode );
+			}
+		}
+
+		private async Task ProcessItems( IReadOnlyList<IReportItem> items, string hierarchyString )
+		{
+			if( !items.Any() )
+				return;
+
+			foreach( var item in items )
+			{
+				await ProcessItem( item, hierarchyString );
+			}
+		}
+
+		private async Task ProcessItem( IReportItem item, string hierarchyString )
+		{
+			if( item is IReportTable table )
+			{
+				await ProcessTable( table, hierarchyString );
+			}
+		}
+
+		private async Task ProcessTable( IReportTable table, string hierarchyString )
+		{
+			foreach( var line in table.Lines )
+			{
+				await ProcessLine( line, hierarchyString );
+			}
+		}
+
+		private async Task ProcessLine( IReportTableLine line, string hierarchyString )
+		{
+			if( line.Table != null )
+			{
+				await ProcessTable( line.Table, hierarchyString );
+			}
+
+			var item = line.Items.FirstOrDefault( i => i is IReportTableLineItemText || i is IReportTableLineItemCombined );
+
+			if( item != null )
+			{
+				string text = "";
+
+				if( item is IReportTableLineItemText textItem )
+					text = textItem.Text;
+				else if( item is IReportTableLineItemCombined combinedItem )
+					text = combinedItem.Text;
+
+				if( !text.Contains( LineItemString ) )
+				{
+					return;
+				}
+
+				// Text matches that being searched for! Output the result value.
+				await OutputResultValue( line, hierarchyString );
+			}
+		}
+
+		private async Task OutputResultValue( IReportTableLine line, string hierarchyString )
+		{
+			// Assume we are looking for a line that contains just one value and one quantity
+			double value = 0.0;
+			Quantity quantity = Quantity.Unknown;
+			string text = "";
+			bool textFound, valueFound, quantityFound;
+			textFound = valueFound = quantityFound = false;
+			foreach( var item in line.Items )
+			{
+				if( !textFound )
+				{
+					if( item is IReportTableLineItemCombined combinedItem )
+					{
+						text = combinedItem.Text;
+						textFound = true;
+					}
+					else if( item is IReportTableLineItemText textItem )
+					{
+						text = textItem.Text;
+						textFound = true;
+					}
+				}
+
+				if( !valueFound )
+				{
+					if( item is IReportTableLineItemCombined combinedItem )
+					{
+						value = combinedItem.Value;
+						valueFound = true;
+					}
+					else if( item is IReportTableLineItemDoubleValue doubleItem )
+					{
+						value = doubleItem.Value;
+						valueFound = true;
+					}
+					else if( item is IReportTableLineItemLongValue longItem )
+					{
+						value = longItem.Value;
+						valueFound = true;
+					}
+				}
+
+				if( !quantityFound )
+				{
+					if( item is IReportTableLineItemCombined combinedItem )
+					{
+						quantity = combinedItem.Quantity;
+						quantityFound = true;
+					}
+					else if( item is IReportTableLineItemQuantity quantityItem )
+					{
+						quantity = quantityItem.Quantity;
+						quantityFound = true;
+					}
+				}
+			}
+
+			if( !valueFound || !quantityFound )
+			{
+				// Not a valid value to output
+				return;
+			}
+
+			var units = await UnitConverter.GetUnitsAsync( quantity, SystemType.Metric );
+			var unit = units.First();
+
+			if( unit != null )
+			{
+				Console.WriteLine( hierarchyString );
+
+				Console.WriteLine( $"\t{text} {(await UnitConverter.FromBaseAsync( new[] { value }, unit )).First()} [{unit.Name}]" );
+			}
+		}
+
+		#endregion
+	}
+}
